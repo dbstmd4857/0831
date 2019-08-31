e5b7ba2075ff9b987ecdcfcb7e2c43ccca43e81e
dbstmd4857 <54461209+dbstmd4857@users.noreply.github.com>
2019-08-31 11:06:55 +0900

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+print("{:s}".format('l\m enjoying learing pyton'))
+
+print("{0:s}".format("this is a long string.without the backslash\
+it would run off of the page on the right in the taxt editor and be very\
+difficult in read and edit. by using the backslash you can split the long\
+string into smaller strings on seperate lines so that the whole string is easy\
+to view in the editor."))
+
+print("{0:s}".format("'you can use tripe single quotes for multi-line comment strings'"))
+
+print("{:s}".format("""you can also use tripe double quotes
+for multi-line comment strings."""))
